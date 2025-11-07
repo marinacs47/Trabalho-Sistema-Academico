6465d530d0aebd3519469a65b3cea172d8eb3e5c
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-07 16:28:29 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,13 @@
+package sistema_academico.modelo;
+
+public class Pessoa {
+	private String nome;
+
+	public Pessoa(String nome){
+		this.nome = nome;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+}
---
+++ Professor.java
@@ -0,0 +1,35 @@
+package sistema_academico.modelo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Professor extends Pessoa{
+	private int siape;
+	private List<Professor> professores = new ArrayList<>(); 
+
+	public Professor(int siape, String nome){
+		super(nome);
+		this.siape = siape;
+	}
+
+	public int getSiape(){
+		return siape;
+	}
+
+	public List<Professor> getProfessores(){
+		return professores;
+	}
+
+	public void inserir(Professor professor){
+		professores.add(professor);
+	}
+
+	public void remover(Professor professor){
+		professores.remove(professor);
+	}
+
+	/*public Professor obter(Professor professor){
+		
+	}*/
+
+}
