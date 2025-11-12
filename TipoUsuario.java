459d2d8eefdecce6991934d379e4f81f2030b48b
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-12 11:07:08 -0300

    Atualizar o TipoUsuario.java

---
+++ TipoUsuario.java
@@ -0,0 +1,28 @@
+package sistema_academico.modelo
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class TipoUsuario {
+	private String nome;
+	private List<Funcionalidade> funcionalidades = new arrayList<>;
+
+	public TipoUsuario(String nome) {
+		this.nome = nome;
+	}
+
+	public String getNome(){
+		return nome;
+	}
+
+	public List<Funcionalidade> getFuncionalidades(){
+		return funcionalidades;
+	}
+
+	public void definirFuncionalidades(){
+
+	}
+
+
+
+}
