5b50656d74014faa3479ff058e75d6556533e77d
Marina Cruvinel Saraiva <marina.cs.0407@gmail.com>
2025-11-18 19:39:39 -0300

    Atualizar o Usuario.java

--- Usuario.java
+++ Usuario.java
@@ -11,27 +11,13 @@ class Usuario implements IAutenticacao(){
 	private static List<Usuario> usuarios = new ArrayList<>();
 
 	public Usuario(String nome, String login, String senha, TipoUsuario tipoUsuario){
-		super(login, senha);
+		this.login = login;
+   this.senha = senha;
 		this.nome = nome;
 		this.tipoUsuario = tipoUsuario;
 	}
 
 	static{
-		TipoUsuario Administrador = new TipoUsuario("Administrador");
-		TipoUsuario Professor = new TipoUsuario("Professor");
-		TipoUsuario Aluno = new TipoUsuario("Aluno");
-		TipoUsuario Turma = new TipoUsuario("Turma");
-
-		usuarios.add(new Usuario("ADMIN", Administrador));
-		usuarios.add(new Usuario("Professor1", Professor));
-		usuarios.add(new Usuario("Professor2", Professor));
-		usuarios.add(new Usuario("Aluno1", Aluno));
-		usuarios.add(new Usuario("Aluno2", Aluno));
-		usuarios.add(new Usuario("Aluno3", Aluno));
-		usuarios.add(new Usuario("Aluno4", Aluno));
-		usuarios.add(new Usuario("Aluno5", Aluno));
-		usuarios.add(new Usuario("Turma1", Turma));
-												
 	}
 
 	public getTipoUsuario getTipoUsuario(){
