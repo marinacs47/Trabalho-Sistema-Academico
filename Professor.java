e67499ac9c205d14edd13c9488d1ea2079c1f081
Miguel Arcanjo Nascimento Meriguete <miguelmeriguete@gmail.com>
2025-11-21 23:15:31 -0300

    Professor.java

--- Professor.java
+++ Professor.java
@@ -1,4 +1,4 @@
-package modelo.sistema_academico;
+package sistema_academico.modelo;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -40,3 +40,4 @@ public class Professor extends Pessoa {
     }
     
 }
+
